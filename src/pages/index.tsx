18bdf0efff3a1c5dda992a443f34cebad7b80f9e
Kristián Řehoř <kmancz.kiki@gmail.com>
2022-07-25 15:20:52 +0200

    Cleaning project files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,10 @@
-import type { NextPage, GetServerSidePropsContext } from "next";
-import { FormEvent, useState } from "react";
+import type { NextPage } from "next";
 import Head from "next/head";
-import { trpc } from "../utils/trpc";
 import AuthBar from "../components/AuthBar";
-
-type TechnologyCardProps = {
-  name: string;
-  description: string;
-  documentation: string;
-};
+import { useSession } from "next-auth/react";
 
 const Home: NextPage = () => {
-  const hello = trpc.useQuery(["example.hello", { text: "Github" }]);
-  const auth = trpc.useQuery(["auth.getSecretMessage"]);
+  const session = useSession();
 
   return (
     <>
@@ -25,72 +17,11 @@ const Home: NextPage = () => {
       <main className="container mx-auto flex flex-col items-center justify-center h-screen p-4">
         <AuthBar />
         <h1 className="text-5xl md:text-[5rem] leading-normal font-extrabold text-gray-700">
-          Create <span className="text-purple-300">T3</span> App
+          Welcome to <span className="text-purple-300">Linker</span>{" "}
+          {session.data?.user?.name}
         </h1>
-        <TestingForm />
-        <MessagesList />
-        <p>{auth.data}</p>
-        <div className="pt-6 text-2xl text-blue-500 flex justify-center items-center w-full">
-          {hello.data ? <p>{hello.data.greeting}</p> : <p>Loading..</p>}
-        </div>
       </main>
     </>
-  );
-};
-
-export function MessagesList() {
-  const { data } = trpc.useQuery(["example.getAllMessages"]);
-  return (
-    <ul>
-      {data ? (
-        data.map((message) => <li key={message.id}>{message.message}</li>)
-      ) : (
-        <p>Loading...</p>
-      )}
-    </ul>
-  );
-}
-
-function TestingForm() {
-  const [input, setInput] = useState<string>("");
-  const { mutate } = trpc.useMutation("example.storeMessage");
-
-  function handleSubmit(e: FormEvent) {
-    e.preventDefault();
-    mutate({ message: input });
-  }
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        className="border"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      />
-      <button type="submit">Send</button>
-    </form>
-  );
-}
-
-const TechnologyCard = ({
-  name,
-  description,
-  documentation,
-}: TechnologyCardProps) => {
-  return (
-    <section className="flex flex-col justify-center p-6 duration-500 border-2 border-gray-500 rounded shadow-xl motion-safe:hover:scale-105">
-      <h2 className="text-lg text-gray-700">{name}</h2>
-      <p className="text-sm text-gray-600">{description}</p>
-      <a
-        className="mt-3 text-sm underline text-violet-500 decoration-dotted underline-offset-2"
-        href={documentation}
-        target="_blank"
-        rel="noreferrer"
-      >
-        Documentation
-      </a>
-    </section>
   );
 };
 
