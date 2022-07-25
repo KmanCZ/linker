6713b35f583e962f6c28663de2f63be96218a8a2
Kristián Řehoř <kmancz.kiki@gmail.com>
2022-07-25 18:56:54 +0200

    Linker Card styling

--- src/components/LinkerBoard.tsx
+++ src/components/LinkerBoard.tsx
@@ -3,7 +3,7 @@ import NewLinkerButton from "./NewLinkerButton";
 
 export default function LinkerBoard() {
   return (
-    <section className="min-h-[50rem] rounded-xl m-5 p-5 bg-slate-50 grid grid-cols-5 justify-items-center gap-y-7">
+    <section className="min-h-[50rem] rounded-xl m-5 p-5 bg-slate-100 grid grid-cols-5 justify-items-center gap-y-7">
       <LinkerCard />
       <LinkerCard />
       <LinkerCard />
--- src/components/LinkerCard.tsx
+++ src/components/LinkerCard.tsx
@@ -1,5 +1,16 @@
 export default function LinkerCard() {
   return (
-    <div className="inline-block w-[20rem] h-[20rem] bg-white shadow-md hover:shadow-xl rounded-lg"></div>
+    <div className="inline-block w-[20rem] h-[20rem] bg-white bg-opacity-30 shadow-md hover:shadow-xl rounded-lg">
+      <div className="bg-yellow-500 h-3/5 rounded-t-lg">Prewiew picture</div>
+      <div className="p-4 h-2/5">
+        <h2 className="font-bold text-2xl text-slate-600">List name</h2>
+        <p className="line-clamp-2 mt-2">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+          debitis nulla eligendi recusandae? Placeat maxime inventore, fugit
+          voluptatibus aperiam sit perferendis voluptas, aut doloribus modi
+          consectetur temporibus ut aliquid nobis?
+        </p>
+      </div>
+    </div>
   );
 }
